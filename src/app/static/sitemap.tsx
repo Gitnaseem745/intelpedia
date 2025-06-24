58ec1d0a2bb533e21250b0efb4812f3b55e5d74e
Gitnaseem745 <mrnaseem745@gmail.com>
2025-06-24 16:56:01 +0530

    feat: update sitemap to include privacy-policy and terms-of-service paths

--- src/app/static/sitemap.tsx
+++ src/app/static/sitemap.tsx
@@ -2,7 +2,7 @@ import { config } from "@/config";
 import type { MetadataRoute } from "next";
 import urlJoin from "url-join";
 
-const staticPaths = ["about"];
+const staticPaths = ["about", "privacy-policy", "terms-of-service"];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const paths = staticPaths.map((path) => ({
