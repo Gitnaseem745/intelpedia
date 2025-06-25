7ced1de7e1745bfbb3d39c8949d515989e917cb5
Gitnaseem745 <mrnaseem745@gmail.com>
2025-06-25 16:09:56 +0530

    fix: update sitemap URLs to use 'www' subdomain and adjust static paths in sitemap generation

--- src/app/static/sitemap.tsx
+++ src/app/static/sitemap.tsx
@@ -2,13 +2,13 @@ import { config } from "@/config";
 import type { MetadataRoute } from "next";
 import urlJoin from "url-join";
 
-const staticPaths = ["about", "privacy-policy", "terms-of-service"];
+const staticPaths = ["", "about", "privacy-policy", "terms-of-service"];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const paths = staticPaths.map((path) => ({
-    url: urlJoin(config.baseUrl, path),
+    url: path === "" ? config.baseUrl : urlJoin(config.baseUrl, path),
     lastModified: new Date(),
-    priority: 0.9,
+    priority: path === "" ? 1.0 : 0.9,
   }));
   return paths;
 }
